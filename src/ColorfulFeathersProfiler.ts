25c9620cf5fadcbe3bf13a9c363978a4fc58e592
Jørgen Vatle <Jorgen@WcAServices.net>
2023-01-08 03:44:38 +0100

    Update text format for ColorfulFeathersProfiler.ts

--- src/ColorfulFeathersProfiler.ts
+++ src/ColorfulFeathersProfiler.ts
@@ -1,10 +1,9 @@
-import { Application, HookContext } from '@feathersjs/feathers';
+import { Application, ServiceMethods } from '@feathersjs/feathers';
 import Chalk from 'chalk';
-import { get } from 'lodash';
 import { assignColor } from './Utilities/ColorPicker';
-import { profiler as FeathersProfiler, timestamp } from 'feathers-profiler';
+import { getPending, profiler as FeathersProfiler, ProfilerContext, timestamp } from 'feathers-profiler';
 
-export default function ColorfulFeathersProfiler({ enabled = true, logger = console }: ProfilerOptions) {
+export default function ColorfulFeathersProfiler({ enabled = true, logger = console, logStyle = 'string' }: ProfilerOptions) {
     return (App: Application) => {
         if (!enabled) {
             logger.warn('ColorfulFeathersProfiler has been disabled!')
@@ -12,24 +11,63 @@ export default function ColorfulFeathersProfiler({ enabled = true, logger = cons
         
         const profiler = FeathersProfiler({
             logMsg(hook) {
-                const elapsed = Math.round(hook._log.elapsed / 1e5) / 10;
-                const route = hook.params.provider ? assignColor(hook._log.route) : hook._log.route;
-                const provider = Chalk.yellowBright(get(hook.params, 'provider', Chalk.grey('server')));
-                
-                const header = `${timestamp()} ${route}::${hook.method}`;
-                const trailer = `(${provider}) ${elapsed} ms - ${pending} pending`;
-                
-                let logMessage = `${header} ${trailer}`;
-        
-                if (hook.error) {
-                    logMessage += ` - ${Chalk.red('FAILED')} ${hook.original?.type} ${hook.error.message || ''}`;
+                const context = parseContext(hook);
+    
+                if (logStyle === 'object') {
+                    return context;
                 }
-        
-                return logMessage;
+    
+                if (context.provider !== 'server') {
+                    context.provider = Chalk.yellowBright(context.provider);
+                    context.route = assignColor(context.route);
+                }
+    
+    
+                let time = `${timestamp()}`
+                let header = `${context.route}::${context.method}`;
+                let trailer = `(${context.provider}) ${context.duration} ms - ${getPending()} pending`;
+                let error = '';
+    
+                if (context.result.error) {
+                    error = `\n [${context.hook.type}: ${context.method}]\n   ${context.result.error.stack || ''}`;
+                    header = Chalk.red(`[ERROR] ${header}`);
+                }
+    
+                return `${time} ${header} ${trailer} ${error}`;
             },
         })
         
         return App.configure(profiler);
+    }
+}
+
+function parseContext(hook: ProfilerContext): ParsedContext {
+    return {
+        duration: Math.round(hook._log.elapsed / 1e5) / 10,
+        route: hook._log.route,
+        provider: hook.params.provider || 'server',
+        method: hook.method,
+        hook: {
+            type: hook.original?.type || hook.type,
+        },
+        result: {
+            status: hook.statusCode,
+            error: hook.error,
+        }
+    }
+}
+
+interface ParsedContext {
+    duration: number;
+    route: string;
+    provider: 'server' | 'rest' | 'socketio' | string;
+    method: keyof ServiceMethods<any>;
+    hook: {
+        type: 'before' | 'after' | 'error';
+    };
+    result: {
+        error?: Error;
+        status?: number;
     }
 }
 
@@ -41,6 +79,7 @@ let pending = 0;
 interface ProfilerOptions {
     enabled: boolean;
     logger: CompatibleLogger;
+    logStyle?: 'object' | 'string';
 }
 
 
