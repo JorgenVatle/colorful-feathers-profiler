17235d99e2d5bde7b0f65d32186ea26da2840264
Jørgen Vatle <Jorgen@WcAServices.net>
2023-01-08 01:45:29 +0100

    Make feathers-profiler options optional.

--- src/Types/feathers-profiler/index.d.ts
+++ src/Types/feathers-profiler/index.d.ts
@@ -2,11 +2,11 @@ declare module 'feathers-profiler' {
     import { HookContext } from '@feathersjs/feathers';
     
     interface ProfilerOptions {
-        logger: null | {
+        logger?: null | {
             log: (message: any) => void;
         }
-        logMsg(hook: HookContext): any;
-        stats: 'none' | 'total' | 'detail' | null,
+        logMsg?: (hook: HookContext) => any;
+        stats?: 'none' | 'total' | 'detail' | null,
     }
     
     function profiler(options: ProfilerOptions);
