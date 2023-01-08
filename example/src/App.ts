aa4cbeb24d0c4be69491efe5564c160835d453e1
Jørgen Vatle <Jorgen@WcAServices.net>
2023-01-08 01:59:55 +0100

    Update App construction in example.

--- example/src/App.ts
+++ example/src/App.ts
@@ -1,16 +1,20 @@
 import Feathers, { Id, Params } from '@feathersjs/feathers';
 import ColorfulFeathersProfiler from '../../src';
 
-const App = Feathers();
-App.use('test-service', new class {
-    async get(id: Id) {
-        return `Fetching ${id}`;
-    }
+const services = {
+    'test-service': new class {
+        async get(id: Id) {
+            return `Fetching ${id}`;
+        }
     
-    async find(params: Params) {
-        return `Finding with ${JSON.stringify({ params })}`
+        async find(params: Params) {
+            return `Finding with ${JSON.stringify({ params })}`
+        }
     }
-});
+} as const;
+
+const App = Feathers<typeof services>();
+
 
 App.configure(ColorfulFeathersProfiler({
     enabled: true,
