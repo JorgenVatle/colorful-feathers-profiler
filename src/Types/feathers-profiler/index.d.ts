4e7ecb0ea9bb7b463e7b44550d8fa1fa9d5583f4
Jørgen Vatle <Jorgen@WcAServices.net>
2023-01-08 02:21:29 +0100

    Expand profiler hook context.

--- src/Types/feathers-profiler/index.d.ts
+++ src/Types/feathers-profiler/index.d.ts
@@ -1,11 +1,21 @@
 declare module 'feathers-profiler' {
     import { HookContext } from '@feathersjs/feathers';
     
+    interface ProfilerContext extends HookContext {
+        _log: {
+            route: string;
+            key: '_total';
+            hrtime: [number, number];
+            elapsed: number;
+        };
+        original?: unknown;
+    }
+    
     interface ProfilerOptions {
         logger?: null | {
             log: (message: any) => void;
         }
-        logMsg?: (hook: HookContext) => any;
+        logMsg?: (hook: ProfilerContext) => any;
         stats?: 'none' | 'total' | 'detail' | null,
     }
     
